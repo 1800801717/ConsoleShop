5258ea90914ae929d15a53a6cac0b8025e0a7470
lenovo <1109018858@qq.com>
2020-12-31 20:25:30 +0800

    添加“table”购物车

--- src/Test.java
+++ src/Test.java
@@ -19,6 +19,31 @@ public class Test {
                 if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
                     System.out.println("登录成功");
                     bo=false;
+                    ReadProductExcel readProductExcel=new ReadProductExcel();
+                    Product products[]=readProductExcel.readExcel(inProduct);
+                    for(Product product:products)
+                    {
+                        System.out.print(product.getpId());
+                        System.out.print("\t"+product.getpName());
+                        System.out.print("\t"+product.getPrice());
+                        System.out.println("\t"+product.getpDesc());
+                    }
+                    System.out.println("请输入商品ID把该商品加入购物车");
+                    String pId=sc.next();
+
+
+                    int count=0;
+                    Product products[]=new Product[3];//创建购物车用数组模拟
+                    System.out.println("请输入商品ID，把商品加入购物车:");
+                    String pId=sc.next();
+                    ReadProductExcel readProductExcel1=new ReadProductExcel();
+                    inPro=null;
+                    inPro=Class.forName("Test").getResourceAsSteam(name:"/product.xlsx");//表示的就是classpath
+                    Product product=readProductExcel1.getProductById(pId,inPro);
+                    if(product!=null)
+                    {
+                        productes[count++]=product;
+                    }
                     break;
                 } else {
                     System.out.println("登录失败");
--- src/table.java
+++ src/table.java
@@ -2,6 +2,19 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class table {
+    ReadProductExcel readProductExcel=new ReadProductExcel();
+    Product products[]=readProductExcel.readExcel(inProduct);
+    for(Product product:products)
+    {
+        System.out.print(product.getpId());
+        System.out.print("\t"+product.getpName());
+        System.out.print("\t"+product.getPrice());
+        System.out.println("\t"+product.getpDesc());
+    }
+    System.out.println("请输入商品ID把该商品加入购物车");
+    String pId=sc.next();
+
+
     int count=0;
     Product products[]=new Product[3];//创建购物车用数组模拟
     System.out.println("请输入商品ID，把商品加入购物车:");
